7f1b7bead28a39f7f73800cb38b75d24cbc2bbe4
satya918 <satyamohanty918@gmail.com>
2023-09-28 16:06:21 +0530

    cors commit

--- src/main/java/com/Jenkins_Api_application/JenkinsApiApplication.java
+++ src/main/java/com/Jenkins_Api_application/JenkinsApiApplication.java
@@ -2,6 +2,9 @@ package com.Jenkins_Api_application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class JenkinsApiApplication {
@@ -9,5 +12,22 @@ public class JenkinsApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsApiApplication.class, args);
 	}
+		
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+				.allowedOrigins("http://localhost:3000")
+				.allowedOrigins("http://13.234.23.179:3001")
+				.allowedOrigins("http://172.16.20.70:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
+                .allowCredentials(true)
+                .maxAge(3600); // Max age of preflight request;
+			}
+		};
+	}
 }
