3a05be99866aa647dd18aaca1fd4cb29c68c2c20
satya918 <satyamohanty918@gmail.com>
2023-09-29 10:57:48 +0530

    remove cors

--- src/main/java/com/Jenkins_Api_application/JenkinsApiApplication.java
+++ src/main/java/com/Jenkins_Api_application/JenkinsApiApplication.java
@@ -12,22 +12,8 @@ public class JenkinsApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsApiApplication.class, args);
 	}
-		
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-				.allowedOrigins("http://localhost:3000")
-				.allowedOrigins("http://13.234.23.179:3001")
-				.allowedOrigins("http://172.16.20.70:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
-                .allowCredentials(true)
-                .maxAge(3600); // Max age of preflight request;
-			}
-		};
+	
 	}
-}
+
+	
+
